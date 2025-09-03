9f22156f4b0aa6842e4343894b3b3c7d544bee88
JiomLan <jiomlan1114@gmail.com>
2025-09-03 11:09:17 +0800

    how to use __gcd(a,b)

--- JiomLan_CppLearning/CppLevel1/Syntax04.cpp
+++ JiomLan_CppLearning/CppLevel1/Syntax04.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
-#include <cmath>
+#include <algorithm>
 using namespace std;
-//P2415 集合求和
+//P1888 三角函数
 /*
 输入一组勾股数 a,b,c 用分数格式输出其较小锐角的正弦值。（要求约分。）
 */
-
+int a,b,c;
+int m,mid,M;
 int main(){
+    cin >> a >> b >> c;
+    M = max(max(a,b),c);
+    m = min(min(a,b),c);
+    //mid = a + b + c - M - m;
+    int g =__gcd(M,m);
+    cout << (m/g) << "/" << (M/g);
+
+
+    return 0;
 }
