0e5e143042e82d945a529b645161d444850b7014
JiomLan <65555262+youcharamahua@users.noreply.github.com>
2025-09-27 22:22:17 +0800

    .gitignore addition

--- JiomLan_CppLearning/CppLevel1/Pointers.cpp
+++ JiomLan_CppLearning/CppLevel1/Pointers.cpp
@@ -3,13 +3,16 @@ using namespace std;
 
 int main(){
     int a = 5;
+    // &a -> 表示成地址 (取址符)
     cout << "Value of a: " << a << endl;
     cout << "Address of a: " << &a << endl;
+    // *变量名 -> 指针变量 
     int *intpoint;
     intpoint = &a;
 
     cout << "Value of p: " << intpoint << endl;
     cout << "Value pointed by p: " << *intpoint << endl;
+    cout << "Address of p: " << &intpoint << endl;
 
     int *ptr = NULL;
     cout << "Value of ptr: " << ptr << endl;
