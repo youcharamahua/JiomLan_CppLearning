d2f4ef02284b097cd6a606bf197335f6bf444472
JiomLan <65555262+youcharamahua@users.noreply.github.com>
2025-09-30 23:20:06 +0800

    rewrite

--- JiomLan_CppLearning/CppLevel2/DynamicProgramming/dp04_P5734.cpp
+++ JiomLan_CppLearning/CppLevel2/DynamicProgramming/dp04_P5734.cpp
@@ -3,20 +3,15 @@ using namespace std;
 int a[5],i,j,k,sum,t,homework[21],dp[2501];
 int main(){
     for(i=1;i<=4;i++)
-        cin>>a[i]; 
+        cin >> a[i]; 
     for(i=1;i<=4;i++){
-        sum=0;    
-        for(j=1;j<=a[i];j++){
-            cin>>homework[j];
-            sum+=homework[j];
-        }
-        for(j=1;j<=a[i];j++)
-            for(k=sum/2;k>=homework[j];k--)
-                dp[k]=max(dp[k],dp[k-homework[j]]+homework[j]);
-        t+=sum-dp[sum/2];
-
-        for(j=1;j<=sum/2;j++)
-            dp[j]=0;
+    	sum = 0;
+    	for(j=1;j<=a[i];j++){
+    		cin >> homework[j];
+			sum += homework[j];
+		}
+		for()
+		
     }
     cout<<t;
     return 0;
