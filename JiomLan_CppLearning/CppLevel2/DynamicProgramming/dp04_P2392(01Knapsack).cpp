1204bd31352b51609da253bb06ab7087f9776366
JiomLan <65555262+youcharamahua@users.noreply.github.com>
2025-10-01 01:27:14 +0800

    01Knapsack

--- JiomLan_CppLearning/CppLevel2/DynamicProgramming/dp04_P2392(01Knapsack).cpp
+++ JiomLan_CppLearning/CppLevel2/DynamicProgramming/dp04_P2392(01Knapsack).cpp
@@ -10,8 +10,15 @@ int main(){
     		cin >> homework[j];
 			sum += homework[j];
 		}
-		for()
-		
+		for(j=1;j<=a[i];j++){
+			for(k=sum/2;k>=homework[j];k--){
+				dp[k] = max(dp[k],dp[k-homework[j]]+homework[j]);
+			}				
+		}
+		t += sum - dp[sum/2];
+		for(j=1;j<=sum/2;j++){
+			dp[j] = 0;
+		}
     }
     cout<<t;
     return 0;
